427ea29fdc08dd1edad7a346a87a99a9f0e42032
Alex1234512 <169060474+Alex1234512@users.noreply.github.com>
2024-06-19 20:59:07 +0400

    commit

---
+++ level 10/Homework/homework.py
@@ -0,0 +1,73 @@
+# დავალება 2
+
+for i in range(1, 100 , 2):
+    print(i)
+
+# დავალება 3
+
+while num < 1000:
+    num = num + 1
+    print(num)
+
+# დავალება 4
+
+sun = 0
+num = 0
+
+while num < 9:
+    num = num + 1
+    sum = sum + num
+
+# დავალება 5
+# while loop
+# #1
+num = 23456
+
+while num < 23456:
+    num = num + 3
+    print(num)
+
+# #2
+num = 0
+
+while num > 5:
+    print(num)
+
+# #3
+
+
+
+
+
+
+# for loop
+# #1
+for i in range(1, 100 , 2):
+        print(i)
+
+# #2
+for i in range(1, 100 , 1):
+        print(i)
+
+# #3
+for i in range(1, -100 , -1):
+        print(i)
+
+# #4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
